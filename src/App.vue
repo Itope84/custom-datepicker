d81f99cac168c7669bc7c4fb493440fa57ea54fb
c-zv <zv.const@gmail.com>
2021-03-12 14:36:56 +0000

    project setup

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <span class="message">Have fun!</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+});
+</script>
+
+<style lang="scss">
+@import "./buefyStyles";
+
+.app {
+  height: 100vh;
+  background: #24292e;
+}
+
+.message {
+  color: #ffcc0d;
+  font-size: 4rem;
+  background: none;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
