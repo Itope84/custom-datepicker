08e61ef91a11f1fa4ba451cb8513ea1bfbc9fbc1
Temitope Ilesanmi <topephysics@gmail.com>
2021-03-23 12:48:47 +0100

    added mounting tests

---
+++ tests/unit/datepicker.spec.ts
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils';
+import DatePicker from '../../src/library';
+
+test('mounts and displays the input field and 2 buttons', () => {
+  // mount() returns a wrapped Vue component we can interact with
+  const wrapper = mount(DatePicker, {
+    propsData: {
+      // msg: 'Hello world',
+    },
+  });
+
+  // Assert the rendered text of the component
+  // expect(wrapper.text()).toContain('Hello world');
+  expect(wrapper.element.querySelectorAll('button').length).toEqual(2);
+  expect(wrapper.element.querySelectorAll('input').length).toEqual(1);
+});
+
+test('Datepicker is hidden by default', () => {
+  // mount() returns a wrapped Vue component we can interact with
+  const wrapper = mount(DatePicker, {
+    propsData: {
+      // msg: 'Hello world',
+    },
+  });
+
+  // Assert the rendered text of the component
+  // expect(wrapper.text()).toContain('Hello world');
+  expect(wrapper.vm.$data.showPicker).toEqual(false);
+  expect(wrapper.element.querySelectorAll('.j-datepicker--picker').length).toEqual(0);
+});
